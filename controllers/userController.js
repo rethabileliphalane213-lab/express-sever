ec5dc9caf4f90fab0ecba0cbe396c8a1ef2de285
Rethabile <rethabileliphalane213@gmail.com>
2026-05-05 11:02:34 +0000

    feat: add sanitize

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,19 @@
 
-const userStorage=require("./storages/userStorage")
+const userStorage=require("../storages/userStorage")
+const {body, validationResult, matchedData}=require(express-validator)
+
+const alphaError="Must only contain aplhabets"
+const lengthError="must be between 1 to 10 characters"
+
+const validateUser=[
+    body("firstName")
+    .trim()
+    .isAlpha().withMessage(`first Name ${alphaError}`)
+    .isLength({min:1,max:10}).withMessage(`Last Name ${lengthError}`),
+    body("lastName").trim()
+    .isAlpha().withMessage("Last Name ${alphaError}")
+    .isLength({min:1,max10}).withMessage(`Last Name ${lengthError}`)
+]
 
 
 exports.userListGet=(req,res)=>{
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,8 @@ const {Router}=require("express")
 const userController=require("../controllers/userController")
 const userRouter=Router()
 
-userRouter.get("/",userController.usersLISTget)
+userRouter.get("/",userController.userListGet)
 userRouter.get("/create", userController.usersCreateGet);
 userRouter.post("/create", userController.usersCreatePost);
 
-module.export=userRouter
+module.exports=userRouter
